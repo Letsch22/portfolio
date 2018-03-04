d5e97b751aec01785d531ca66c077fa454594d20
Daniel Letscher <dpletscher@outlook.com>
2018-03-03 16:16:53 -0800

    add separate project class

--- client/src/projects/Projects.js
+++ client/src/projects/Projects.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import { Card, Container, Header, Icon } from "semantic-ui-react";
+import { Card, Container, Header, Transition, Visibility } from "semantic-ui-react";
+import Project from "./project/Project";
 
 class Projects extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { projects: [] };
+        this.state = { projects: [], visible: false };
+        this.flipCards = this.flipCards.bind(this);
+    }
+
+    flipCards() {
+        this.setState({ visible: true });
     }
 
     componentDidMount() {
@@ -14,40 +20,33 @@ class Projects extends Component {
             .then(projects => this.setState({projects}));
     }
 
-    renderCard(project, i) {
-        var extra = null;
-
-        if (project.extra != null) {
-            extra = (
-                <a href={project.extraLink} target={project.header}>
-                    <Icon name={project.extraIcon}/>
-                    {project.extra}
-                </a>
-            );
-        }
-
+    renderProject(project, i) {
         return (
-            <Card
+            <Project
                 key={i}
                 header={project.header}
                 image={project.image}
-                raised={true}
                 meta={project.meta}
                 description={project.description}
-                extra={extra}
+                extra={project.extra}
+                extraIcon={project.extraIcon}
+                extraLink={project.extraLink}
             />
         );
     }
 
     render() {
         return (
-            <div className="projects" id="projects">
+            <div id="projects">
                 <Container>
                     <Header>Projects</Header>
-                    {/* TODO add dimmer to card image + button */}
-                    <Card.Group itemsPerRow={3} centered textAlign="left">
-                        {this.state.projects.map((project, i) => this.renderCard(project, i))}
-                    </Card.Group>
+                    <Visibility onBottomVisible={this.flipCards} once>
+                        <Transition animation="vertical flip" visible={this.state.visible} mountOnShow={false} duration={750}>
+                            <Card.Group itemsPerRow={3} centered textAlign="left">
+                                {this.state.projects.map((project, i) => this.renderProject(project, i))}
+                            </Card.Group>
+                        </Transition>
+                    </Visibility>
                 </Container>
             </div>
         );
---
+++ client/src/projects/project/Project.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Button, Card, Dimmer, Icon, Image } from "semantic-ui-react";
+import PropTypes from "prop-types";
+
+class Project extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { active: false, visible: false };
+        this.handleHide = this.handleHide.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+    }
+
+    handleShow() {
+        this.setState({ active: true });
+    }
+
+    handleHide() {
+        this.setState({ active: false });
+    }
+
+    render() {
+        const { active } = this.state;
+
+        const content = (
+            <div>
+                <Button inverted color="grey" size="huge" primary>Learn more</Button>
+            </div>
+        );
+
+        var extra = null;
+
+        if (this.props.extra != null) {
+            extra = (
+                <Card.Content extra>
+                    <a href={this.props.extraLink} target={this.props.header}>
+                        <Icon name={this.props.extraIcon}/>
+                        {this.props.extra}
+                    </a>
+                </Card.Content>
+            );
+        }
+
+        return (
+            <Card raised>
+                <Dimmer.Dimmable
+                    as={Image}
+                    blurring
+                    dimmed={active}
+                    dimmer={{ active, content }}
+                    onMouseEnter={this.handleShow}
+                    onMouseLeave={this.handleHide}
+                    src={this.props.image}/>
+                <Card.Content>
+                    <Card.Header>
+                        {this.props.header}
+                    </Card.Header>
+                    <Card.Meta>
+                        {this.props.meta}
+                    </Card.Meta>
+                    <Card.Description>
+                        {this.props.description}
+                    </Card.Description>
+                </Card.Content>
+                {extra}
+            </Card>
+        );
+    }
+}
+
+Project.propTypes = {
+    header: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    meta: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    extra: PropTypes.string,
+    extraLink: PropTypes.string,
+    extraIcon: PropTypes.string
+};
+
+export default Project;
