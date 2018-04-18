0461a2fef85e8e7465e7260146d3de9b878c5e40
Daniel Letscher <dpletscher@outlook.com>
2018-04-17 23:57:30 -0700

    About additions

--- client/src/about/About.js
+++ client/src/about/About.js
@@ -3,15 +3,26 @@ import { Grid, Header } from "semantic-ui-react";
 
 class About extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {about: []};
+    }
+
+    componentDidMount() {
+        fetch("/json/about")
+            .then(res => res.json())
+            .then(about => this.setState({about}));
+    }
+
     render() {
         return(
             <div id="myself">
                 <Grid centered>
-                    <Grid.Column width={6}>
+                    <Grid.Column width={5}>
                     </Grid.Column>
-                    <Grid.Column width={6}>
+                    <Grid.Column width={7}>
                         <Header>Myself</Header>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam elit sed tortor volutpat mattis. Nam ac massa eu est aliquam vehicula vitae ac tortor. Ut commodo turpis venenatis tristique consequat. Aliquam dui eros, consectetur sed hendrerit ut, congue at nibh. Aenean sit amet efficitur mi, sit amet porttitor eros. Nulla facilisi. Aenean ac pretium metus. Donec sem ligula, vestibulum vel velit sed, tristique porta leo.</p>
+                        {this.state.about.map((paragraph, i) => <p key={i}>{paragraph}</p>)}
                     </Grid.Column>
                 </Grid>
             </div>
--- routes/json.js
+++ routes/json.js
@@ -5,7 +5,7 @@ var projects = require("../data/json/projects.json");
 var resume = require("../data/json/resume.json");
 var skills = require("../data/json/skills.json");
 var icons = require("../data/json/icons.json");
-
+var about = require("../data/json/about.json");
 
 router.get("/projects", function (req, res, next) {
     res.send(projects);
@@ -21,6 +21,10 @@ router.get("/skills", function (req, res, next) {
 
 router.get("/icons", function (req, res, next) {
     res.send(icons);
+});
+
+router.get("/about", function (req, res, next) {
+    res.send(about);
 });
 
 module.exports = router;
