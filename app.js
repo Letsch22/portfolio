bc19a203d558f523f6acc3a433baa2368905f0e1
Daniel Letscher <dpletscher@outlook.com>
2018-05-12 14:50:04 -0700

    Express static routing for react frontend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/json", json);
 app.use("/documents", documents);
 
